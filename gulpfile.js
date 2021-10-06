1987eed499f6e213e3150865f46cd7bf614f297d
simon <simonrbrain@yahoo.co.uk>
2021-10-06 13:08:58 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ function watch() {
         server: {
             baseDir: './dest/'
         },
-        reloadDelay: 1800
+        reloadDelay: 2500
     });
     gulp.watch('./src/sass/**/*.sass', css);
     gulp.watch('./src/js/**/*.js', javascript);
