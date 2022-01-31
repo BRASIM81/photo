8c6cb64b5e66e56d503bc1ce136e5db2d742bdfb
simon <simonrbrain@yahoo.co.uk>
2022-01-31 14:02:38 +0000

    added google map

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 window.onload = function(){
 	if(document.cookie.length != '0'){
-		var cookieVal = document.cookie.split("=");
-		if(cookieVal[1] == "yes"){
+		var cookieVal = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((acumulator, [key, value]) => ({
+			...acumulator,
+			[key.trim()]: decodeURIComponent(value)
+		}), {});
+		if(cookieVal.cookieacpt == "yes"){
 			$("#cookies").css({
 				'opacity':'0',
 				'transform':'translate(0,100%)'
@@ -23,3 +26,16 @@ $(document).ready(function(){
 		});
 	});
 });
+function myMap() {
+	var myCenter = new google.maps.LatLng(52.102016, -1.922703);
+	var mapProp = {
+		center: myCenter,
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	var marker = new google.maps.Marker({
+		position: myCenter,
+	});
+	marker.setMap(map);
+}
